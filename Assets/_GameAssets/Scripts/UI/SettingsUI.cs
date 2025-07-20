ad7bc8d2c5ef45d70ebca4b7fb53baedba86803c
Mustafa Yağız ERSOY <yagzersoy@gmail.com>
2025-07-20 16:36:31 +0300

    v0.28 Settings menu ımproved

--- Assets/_GameAssets/Scripts/UI/SettingsUI.cs
+++ Assets/_GameAssets/Scripts/UI/SettingsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Transactions;
 using DG.Tweening;
 using MaskTransitions;
@@ -19,10 +20,18 @@ public class SettingsUI : MonoBehaviour
     [SerializeField] private Button _resumeButton;
     [SerializeField] private Button _mainmenuButton;
 
+    [Header("Sprites")]
+    [SerializeField] private Sprite _musicActiveSprite;
+    [SerializeField] private Sprite _musicPassiveSprite;
+    [SerializeField] private Sprite _soundActiveSprite;
+    [SerializeField] private Sprite _soundPassiveSprite;
     [Header("Settings")]
     [SerializeField] private float _animationDuration;
 
     private Image _blackBackgroundImage;
+
+    private bool _isMusicActive;
+    private bool _isSoundActive;
 
     void Awake()
     {
@@ -37,6 +46,25 @@ public class SettingsUI : MonoBehaviour
             AudioManager.Instance.Play(SoundType.TransitionSound);
             TransitionManager.Instance.LoadLevel(Consts.SceneNames.MENU_SCENE);
         });
+
+        _musicButton.onClick.AddListener(OnMusicButtonClicked);
+        _soundButton.onClick.AddListener(OnSoundButtonClicked);
+    }
+
+    private void OnSoundButtonClicked()
+    {
+        AudioManager.Instance.Play(SoundType.ButtonClickSound);
+        _isSoundActive = !_isSoundActive;
+        _soundButton.image.sprite = _isSoundActive ? _soundActiveSprite : _soundPassiveSprite;
+        AudioManager.Instance.SetSoundEffectsMute(!_isSoundActive);
+    }
+
+    private void OnMusicButtonClicked()
+    {
+        AudioManager.Instance.Play(SoundType.ButtonClickSound);
+        _isMusicActive = !_isMusicActive;
+        _musicButton.image.sprite = _isMusicActive ? _musicActiveSprite : _musicPassiveSprite;
+        BackgroundMusic.Instance.SetMusicMute(!_isMusicActive);
     }
 
     private void OnSettingsButtonClikced()
